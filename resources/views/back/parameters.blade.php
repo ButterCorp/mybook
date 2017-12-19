193f2d5a37c6c252a39a485d1b9522893970c499
Kevin <kev_kevin@hotmail.fr>
2017-12-20 00:44:54 +0100

    test view parameters

--- resources/views/back/parameters.blade.php
+++ resources/views/back/parameters.blade.php
@@ -13,6 +13,13 @@
             <div class="album col s4">
                 <img src="{{ $link["picture"] }}" alt="{{ $graphNode['name'] }}">
             </div>
+            <div class="col s1">
+                <label class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input">
+                    <span class="custom-control-indicator"></span>
+                    <span class="custom-control-description">Check this custom checkbox</span>
+                </label>
+            </div>
         </div>
     @endforeach
 @endforeach
