2cd2b619a8001ce82bf67f2a74961696c322b44f
Kevin <kev_kevin@hotmail.fr>
2017-12-20 00:57:25 +0100

    test view parameters

--- resources/views/back/parameters.blade.php
+++ resources/views/back/parameters.blade.php
@@ -26,6 +26,24 @@
     @endforeach
 @endforeach
 
-<a class="waves-effect waves-light btn">Valider</a>
+<div class="row">
+    <div class="col s12 m7">
+        <div class="card">
+            <div class="card-image">
+                <img src="{{ $link["picture"] }}">
+                <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+                <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+                <a href="#">This is a link</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<a class="waves-effect waves-light btn">Valider</a><br><br><br>
 
 @endsection
