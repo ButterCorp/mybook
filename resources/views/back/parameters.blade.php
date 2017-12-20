bcfde666c41c6517ef8524b5933652fa0a1cddb1
RobinTHOREL <thorelrobin@yahoo.fr>
2017-12-20 12:24:52 +0100

    parameter blade

--- resources/views/back/parameters.blade.php
+++ resources/views/back/parameters.blade.php
@@ -4,33 +4,35 @@
 
 @section('content')
 
-<h2>Configuration</h2>
-<p>
-    Veuillez sélectionner les photos que vous souhaitez afficher sur votre site vitrine,
-    appuyez ensuite sur le bouton valider tout en bas de la page
-</p>
+    <h2>Configuration</h2>
+    <p>
+        Veuillez sélectionner les photos que vous souhaitez afficher sur votre site vitrine,
+        appuyez ensuite sur le bouton valider tout en bas de la page
+    </p>
 
-@foreach ($data as $graphNode)
-    @if(isset($graphNode['photos']))
-            @foreach ($graphNode['photos'] as $link)
-                <div class="album col s4">
-                    <div class="photo-album">
+    <ul class="collapsible popout" data-collapsible="accordion">
+        @foreach ($data as $graphNode)
+            <li>
+                <div class="collapsible-header"><i class="material-icons">filter_drama</i>
+                    {{ $graphNode['name'] }}  </div>
+                <div class="collapsible-body">
+                    @if(isset($graphNode['photos']))
+                        @foreach ($graphNode['photos'] as $link)
+
+
                             @if (isset($link["picture"]) && isset($graphNode['name']))
-                                @if ($loop->first)
-                                    <img src="{{ $link["picture"] }}" alt="{{ $graphNode['name'] }}">
-                                @endif
+                                <div class="col s3">
+                                    <img class="responsive-img materialboxed" data-caption="" src="{{ $link["picture"] }}">
+                                </div>
                             @endif
-                    </div>
-                    <div class="title-album">
-                        @if ($loop->first)
-                            <h3>{{ $graphNode['name'] }}</h3>
-                        @endif
-                    </div>
+
+
+                        @endforeach
+                    @endif
                 </div>
-            @endforeach
-        @endif
-@endforeach
+            </li>
+        @endforeach
+    </ul>
 
-
-<a class="btn" onclick="Materialize.toast('A toi de coder la suite !', 4000)">Valider</a><br><br><br>
+    <a class="btn" onclick="Materialize.toast('A toi de coder la suite !', 4000)">Valider</a><br><br><br>
 @endsection
