73d0d1a99fa959964f39607174aed5e4ab42d5f5
RobinTHOREL <thorelrobin@yahoo.fr>
2017-12-20 10:11:44 +0100

    modify foreach in blade (parameters)

--- resources/views/back/parameters.blade.php
+++ resources/views/back/parameters.blade.php
@@ -11,31 +11,23 @@
 </p>
 
 @foreach ($data as $graphNode)
-
-    <?php
-
-    if(isset($graphNode['photos'])){ ?>
-    @foreach ($graphNode['photos'] as $link)
-        <div class="row">
-            <div class="album col s3">
-                <?php
-                    if (isset($link["picture"]) && isset($graphNode['name'])){
-                        ?>
-
-                        <img src="{{ $link["picture"] }}" alt="{{ $graphNode['name'] }}">
-                    <?php
-                    }
-                ?>
-            </div>
-            <div class="col s3">
-                <p>
-                    <input type="checkbox" id="{{ $link["picture"] }}" />
-                    <label for="{{ $link["picture"] }}">Oui, je veux cette photo</label>
-                </p>
-            </div>
-        </div>
-    @endforeach
-    <?php } ?>
+    @if(isset($graphNode['photos']))
+            @foreach ($graphNode['photos'] as $link)
+                <div class="row">
+                    <div class="album col s3">
+                            @if (isset($link["picture"]) && isset($graphNode['name']))
+                                <img src="{{ $link["picture"] }}" alt="{{ $graphNode['name'] }}">
+                            @endif
+                    </div>
+                    <div class="col s3">
+                        <p>
+                            <input type="checkbox" id="{{ $link["picture"] }}" />
+                            <label for="{{ $link["picture"] }}">Oui, je veux cette photo</label>
+                        </p>
+                    </div>
+                </div>
+            @endforeach
+        @endif
 @endforeach
 
 <a class="btn" onclick="Materialize.toast('A toi de coder la suite !', 4000)">Valider</a><br><br><br>
