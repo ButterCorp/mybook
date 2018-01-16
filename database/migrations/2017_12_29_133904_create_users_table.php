559b059ae2634c6a8c5f4a5ef7c00493e1f363ec
Kevin <kev_kevin@hotmail.fr>
2018-01-16 14:40:26 +0100

    update migrations add foreign keys between albums and users

--- database/migrations/2017_12_29_133904_create_users_table.php
+++ database/migrations/2017_12_29_133904_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigInteger('id');
+            $table->primary('id');
             $table->string('name');
             $table->string('email');
             $table->timestamps();
