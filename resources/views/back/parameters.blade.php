6df408845f9473b96ab213f170bce37fa5d1685e
Kevin <kev_kevin@hotmail.fr>
2018-01-29 20:35:30 +0100

    photos good size in parameters

--- resources/views/back/parameters.blade.php
+++ resources/views/back/parameters.blade.php
@@ -22,9 +22,22 @@
                         <select multiple="multiple" name="{{ $graphNode['id'] }}-{{ $graphNode['name'] }}[]" class="image-picker show-html">
                             @if(isset($graphNode['photos']))
                                 @foreach ($graphNode['photos'] as $link)
+
+
+                                    <?php $source = ""; ?>
+                                    @foreach($link["images"] as $item)
+                                        @if($item["height"] > 300 && $item["height"] < 600 & $item['width'] > 300 & $item['width'] < 600)
+                                            <?php $source = $item["source"]; ?>
+                                        @endif
+                                    @endforeach
+
+                                    @if($source == "")
+                                        <?php $source = $link["images"][0]["source"]; ?>
+                                    @endif
+
                                     @if (isset($link["images"][0]["source"]) && isset($graphNode['name']))
 
-                                        <option data-img-src="{{ $link["images"][0]["source"] }}" value="{{ $link["images"][0]["source"] }}"></option>
+                                        <option data-img-src="{{ $source }}" value="{{ $source }}"></option>
 
                                         <!-- <span class="new badge" data-badge-caption="likes">{{ $link["likes"]->getTotalCount() }}</span> -->
 
