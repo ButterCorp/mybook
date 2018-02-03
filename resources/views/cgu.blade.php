1efb55b4be213b7c16a9668691e875d683c06234
TharshanSivapalan <tharshan.sivapalan@gmail.com>
2018-02-03 18:30:05 +0100

    update page CGU

--- resources/views/cgu.blade.php
+++ resources/views/cgu.blade.php
@@ -1,37 +1,80 @@
-<h1>CGU - MyBook</h1>
-<h3>Politique d’utilisation des données</h3>
-<p>Nous vons donnons la possibilité de créér un site vitrine afin d'augmenter votre visibilité et de partager vos photos</p>
-<p>La présente politique indique le type de données que nous recueillons, l’utilisation que nous en faisons et la façon dont nous les partageons.</p>
+<!DOCTYPE html>
+<html lang="fr">
+   <head>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="style.css" />
+      <title>CGU - MyBook</title>
+   </head>
+   <body>
+      <div id="bloc">
+         <header>
+            <div id="first_title">
+               <div id="logo">
+                  <img src="images/butter.png" alt="ButterCorp" />
+                  <h1>CGU</h1>
+                  <br>
+                  <h2>MyBook</h2>
+               </div>
 
-<h3>Quels types d’informations recueillons-nous ? </h3>
-<p>Nous recueillons les informations suivantes:</p>
-<ul>
-    <li>Votre prénom</li>
-    <li>Votre nom</li>
-    <li>Votre email</li>
-    <li>L'url des photos que vous sélectionnez dans la page parameters de notre application</li>
-    <li>Le nombre de likes et de commentaires de vos photos</li>
-    <li>Et rien de plus...</li>
-</ul>
-<h3>Comment utilisons-nous ces informations ? </h3>
-<p>
-    Notre but est de vous aider à créer un site vitrine attrayant et personnalisé. <br>
-    Pour ce faire nous stockons l'url des photos que vous avez sélectionner dans notre base de données afin de pouvoir afficher vos photos sur votre site vitrin<br>
-    Votre prénom, votre nom et votre email permettent aux visiteurs de votre site qui souhaitent vous contactez <br>
-    Vous avez la possibilité d'effacer ces informations pour avoir un site totalement anonyme
-</p>
-<h3>Comment ces informations sont-elles partagées ? </h3>
-<p>
-    Toutes les informations que vous nous donner sont visible uniquement sur votre site vitrine et nul part ailleurs.<br>
-    Nous ne partageons pas ces informations avec des partenaires tiers.
-</p>
-<h3>Comment puis-je gérer ou supprimer les informations me concernant ? </h3>
-<p>
-    Sur l'application vous avez la possibilité de retirer votre prénom, nom, email, ainsi que toutes les informations relatives à vos photos de notre base de données.<br>
-    Une fois la suppression de ces informations effectuées, elles sont supprimés de manière permanente.
-</p>
-<h3>Comment adresser vos questions à MyBook ?</h3>
-<p>
-    Vous avez la possibilité de nous contacter par mail à l'adresse suivante: xxx@mail.fr <br>
-    Nous vous garantissons une réponse sous 24h.
-</p>
+            </div>
+            
+         </header>
+      
+         <section>
+            <article>
+            <h3>Politique d’utilisation des données</h3>
+            <p>
+               Nous vons donnons la possibilité de créér un site vitrine afin d'augmenter votre visibilité et de partager vos photos
+            </p>
+            <p>
+               La présente politique indique le type de données que nous recueillons, l’utilisation que nous en faisons et la façon dont nous les partageons.
+            </p>
+            <h3>Quels types d’informations recueillons-nous ? </h3>
+            <p>
+               Nous recueillons les informations suivantes:
+            </p>
+            <ul>
+               <li>Votre prénom</li>
+               <li>Votre nom</li>
+               <li>Votre email</li>
+               <li>L'url des photos que vous sélectionnez dans la page parameters de notre application</li>
+               <li>Le nombre de likes et de commentaires de vos photos</li>
+               <li>Et rien de plus...</li>
+            </ul>
+            <h3>Comment utilisons-nous ces informations ? </h3>
+            <p>
+               Notre but est de vous aider à créer un site vitrine attrayant et personnalisé.
+               <br>
+               Pour ce faire nous stockons l'url des photos que vous avez sélectionner dans notre base de données afin de pouvoir afficher vos photos sur votre site vitrin
+               <br>
+               Votre prénom, votre nom et votre email permettent aux visiteurs de votre site qui souhaitent vous contactez
+               <br>
+               Vous avez la possibilité d'effacer ces informations pour avoir un site totalement anonyme
+            </p>
+            <h3>Comment ces informations sont-elles partagées ? </h3>
+            <p>
+               Toutes les informations que vous nous donner sont visible uniquement sur votre site vitrine et nul part ailleurs.
+               <br>
+               Nous ne partageons pas ces informations avec des partenaires tiers.
+            </p>
+            <h3>Comment puis-je gérer ou supprimer les informations me concernant ? </h3>
+            <p>
+               Sur l'application vous avez la possibilité de retirer votre prénom, nom, email, ainsi que toutes les informations relatives à vos photos de notre base de données.
+               <br>
+               Une fois la suppression de ces informations effectuées, elles sont supprimés de manière permanente.
+            </p>
+            </article>
+            <aside>
+            <h3>Comment adresser vos questions à MyBook ?</h3>
+            <p id="butter"><img src="images/butter.png"></p>
+            <p>
+               Vous avez la possibilité de nous contacter par mail à <a href="mailto:butter.brxtn@gmail.com"> l'adresse suivante </a>
+               <br>
+               Nous vous garantissons une réponse sous 24h.
+            </p>
+            </aside>
+         </section>
+         <footer><p>©Copyright 2018 ButterCorp All Rights Reserved</p></footer>
+      </div>
+   </body>
+</html>
